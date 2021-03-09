6afbf783cf3daeb45d0f434d47390e32e972c28e
Prasanna.N <prasannanatarajan3595@gmail.com>
2021-03-09 14:53:53 +0530

    Few changes

--- web/__init__.py
+++ web/__init__.py
@@ -1,13 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "contacts.db"
 
 def create_app():
     from .view import view
+    from .models import contact
+
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '*$@&*(%@(*YHFRUFHOWUHO*@$(FHSUkgfauguo'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     app.register_blueprint(view, url_prefix='/')
+    create_database(app)
     
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     return app
 
     
+def create_database(app):
+    if not path.exists('web/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- web/view.py
+++ web/view.py
@@ -1,11 +1,30 @@
 from flask import Blueprint, render_template, request, flash, jsonify
+from .models import contact
+from . import db
+import json
 
 view = Blueprint('view',__name__)
 
 @view.route('/')
 def home():
+    if request.method == 'POST':
+        name = request.form.get('Name')
+        email = request.form.get('Email')
+        comments = request.form.get('Comments')
+        if len(name)<2:
+            flash('Your name must be aleast 2 characters long', category='error')
+        elif len(email)<3:
+            flash('Enter valid email', category='error')
+        elif len(comment)<1:
+            flash('Your comment must be aleast 1 characters long', category='error')
+        else:
+            new_comment = contact(name=name,email=email,comments=comments)
+            db.session.add(new_comment)
+            db.session.commit()
+            flash('Thanks for commenting on my website!!! Get you soon...', category='success')
+        
     return render_template('home.html')
 
 @view.route('/test')
 def test():
-    return render_template('test.html')
+    return render_template('test.html',data=contact)
