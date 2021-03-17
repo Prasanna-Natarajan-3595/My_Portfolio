1c65b6827d91cc14ba16fbc60b6b68b61b52b042
Prasanna.N <prasannanatarajan3595@gmail.com>
2021-03-17 17:59:08 +0530

    Delete models.py

--- web/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-from sqlalchemy.sql import func
-
-
-class contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10000))
-    email = db.Column(db.String(10000))
-    comments = db.Column(db.String(10000))
