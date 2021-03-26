c4c9268e0e314877637208c335a1236012babe6a
Prasanna <prasannanatarajan3595@gmail.com>
2021-03-26 21:50:01 +0530

    Add files via upload

--- web/__init__.py
+++ web/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from os import path
-from flask_mail import Mail,Message
 
-class app:
-    def __init__(self):
-        pass       
-
-    def createapp(self):
-        from .view import view
-        app = Flask(__name__)
-        app.config['SECRET_KEY'] = '*$@&*(%@(*YHFRUFHOWUHO*@$(FHSUkgfauguo'
-        app.register_blueprint(view, url_prefix='/')
-        return app
-    
+def create_app():
+    from .view import view
+    app = Flask(__name__)
+    app.register_blueprint(view, url_prefix='/')
+    return app
