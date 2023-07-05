dcecedb0f6a950eaec554538fb53c84f6a674e14
Prasanna <prasannanatarajan3595@gmail.com>
2023-07-05 19:38:41 +0530

    working prototype

--- flask/app.py
+++ flask/app.py
@@ -1,31 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify,session
 import chat
 import dotenv,os
 
 dotenv.load_dotenv()
 
-# Set up your OpenAI API credentials
-OPENAI_API_KEY= os.environ.get('OPENAI_API_KEY')
 
 
 app = Flask(__name__)
+
+# Set up your OpenAI API credentials
+OPENAI_API_KEY= os.environ.get('OPENAI_API_KEY')
+app.secret_key = os.environ.get('SECRET_KEY')
+chat_model = chat.chat_api(OPENAI_API_KEY)
 
 # Define your routes and other Flask configurations here
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/answer', methods=['POST'])
+@app.route('/process-command', methods=['POST'])
 def answer():
-    question = request.form['question']
-    # Use the GPT model to generate the answer
-    chat_model = chat.chat_api(OPENAI_API_KEY)
-    base_message = chat.message()
-    base_message.load_persona()
-    answer = chat_model.get_completion(base_message,message=question)
+    command = request.json['command']
+    base_history_messages = session.get('message_history')
+    base_history = chat.message()
+    if base_history_messages:
+        base_history.messages = base_history_messages
+    else:
+        base_history.load_persona()
 
+    answer = chat_model.get_completion(base_history,message=command,update_history=True)
+    session['message_history'] = base_history.messages
     
-    return render_template('answer.html', question=question, answer=answer)
+    return jsonify({'output': answer})
 
 if __name__ == '__main__':
     app.run(debug=True)
